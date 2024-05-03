fbb27551e5fd0ec87cb3b40d5bb5cd0a4e5527ed
仲村士苑 <shionnakamura@nakamurashiens-MacBook-Pro.local>
2024-05-04 01:42:21 +0900

    new

--- 001_syntax/003_loop.js
+++ 001_syntax/003_loop.js
@@ -2,24 +2,40 @@
  *  3.1 0~10までを表示するメソッドを実装してください
  *
  */
-
-function printOneToTen() {
+const number = [0,1,2,3,4,5,6,7,8,9,10]
+function printOneToTen(num) {
+   for(num of number){
+    console.log(num);
+   }
 }
+printOneToTen();
 
 /**
  *  3.2 0~10までの数字を数字の大きい順に表示するメソッドを実装してください
  * */
 
 function printOneToTenDesc() {
+  for(let i=10;i>0;i--)
+  {
+    console.log(i);
+  }
 }
+printOneToTenDesc();
 
 /**
  *  3.3 while文を使って0~10までの数字を小さい順番に表示するメソッドを実装してください
  *
  */
 
+
 function printOneToTenWhile() {
+  let number=0;
+  while(number <=10){
+    console.log(number);
+    number++;
+  }
 }
+printOneToTenWhile();
 
 /**
  *  3.4 while文を使って0~10までの数字を大きい順番に表示するメソッドを実装してください
@@ -27,7 +43,13 @@ function printOneToTenWhile() {
  */
 
 function printOneToTenWhileDesc() {
+  let number = 11
+  while(number>0){
+    number --
+    console.log(number);
+  }
 }
+printOneToTenWhileDesc()
 
 module.exports = {
   printOneToTen,
--- 001_syntax/004_string.js
+++ 001_syntax/004_string.js
@@ -4,7 +4,9 @@
  */
 
 function length(str) {
+  return str.length;
 }
+length('hello');
 
 /**
  *  4.2 文字列のながさが偶数の場合にtrueを返却するメソッドを実装してください
@@ -12,7 +14,13 @@ function length(str) {
  */
 
 function lengthIsEven(str) {
+   return str.length%2 ===0? console.log(true):console.log(false);  
 }
+lengthIsEven('hello')
+lengthIsEven('hell')
+lengthIsEven('hel')
+lengthIsEven('he')
+
 
 /**
  *  4.3 文字列の先頭一文字目を返却するメソッドを実装してください
@@ -20,7 +28,9 @@ function lengthIsEven(str) {
  */
 
 function firstChar(str) {
+  return str[0]
 }
+console.log(firstChar('hello'));
 
 /**
  *  4.4 文字列の末尾一文字目を返却するメソッドを実装してください
@@ -28,7 +38,9 @@ function firstChar(str) {
  */
 
 function lastChar(str) {
+  return  str[str.length -1]
 }
+console.log(lastChar('hello'));
 
 /**
  *  4.5 文字列と二つの数字a, bを渡すとa文字目から, b文字目まで
@@ -37,7 +49,9 @@ function lastChar(str) {
  */
 
 function substring(str, a, b) {
+  return str.substring(a,b+1);
 }
+ console.log(substring('hello',0,5));
 
 /**
  *  4.6 引数に与えられた二つの引数のうち、一つ目の引数の文字列に二つ目の引数の文字列が
@@ -51,7 +65,16 @@ function substring(str, a, b) {
  * */
 
 function isInclude(a, b) {
+   if(a.includes(b)){
+    return true
+   }
+   else{
+    return false
+   }
 }
+console.log(isInclude("workplace", "work"));
+console.log(isInclude("work", "workplace"));
+console.log(isInclude("hogehoge", ""));
 
 /**
  *  4.7 引数で渡された文字列を一文字ずつ表示するメソッドを実装してください
@@ -69,7 +92,11 @@ function isInclude(a, b) {
  */
 
 function printByChar(str) {
+  for(let i=0;i<str.length;i++){
+    console.log(str[i]);
+  }
 }
+printByChar('library');
 
 
 module.exports = {
